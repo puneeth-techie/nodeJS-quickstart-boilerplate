57c72a3533d735059ddc4eccf287aca4c1d466f6
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-04-11 19:53:03 +0530

    Fixed dev dependency.

--- startups/app.js
+++ startups/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { notFound, errorHandler } from "../middlewares/errorHandler.js";
 import homeRoute from "../routes/quickStarterRoute.js";
+import morgan from "morgan";
 
 // init app
 const app = express();
@@ -9,6 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
+
+// Development middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // view engine
 app.set("view engine", "ejs");
